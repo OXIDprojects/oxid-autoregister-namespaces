9bb04e238ebab3318e48c215f4cd424609f49472
Frank Zunderer <info@zunderweb.de>
2018-10-22 14:20:42 +0200

    version 0.0.3
    Add profiling, get rid of notices, add caching

--- metadata.php
+++ metadata.php
@@ -8,11 +8,14 @@ $aModule = array(
         'de'=>'',
         'en'=>'',
     ),
-    'version'     => '0.0.1',
+    'version'     => '0.0.3',
     'url'         => 'http://zunderweb.de',
     'email'       => 'info@zunderweb.de',
     'author'      => 'Zunderweb',
     'extend'      => array(
         'oxconfig' => 'zunderweb/autoregister-namespaces/core/autoregister_namespaces_oxconfig', 
+    ),
+    'settings' => array(
+        array('group' => 'zwar_settings', 'name' => 'blZwarProductionMode', 'type' => 'bool',  'value' => '0'),
     ),
 );
